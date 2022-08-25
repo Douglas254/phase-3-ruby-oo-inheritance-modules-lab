235cf3b26107767a2dabf69c24f60e2ad1b17b2f
Douglas Obara <douglasobara97@gmail.com>
2022-08-25 06:36:58 +0300

    commit tree 487b3afde5740040503d6607ed1c37931610f2a9

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,34 @@
 require 'pry'
 
+# Song
+# can initialize a song
+# can have a name
+# converts its name to a url friendly parameter
+# has an artist
+# Class methods
+#   keeps track of the songs that have been created
+#   can count how many songs have been created
+#   can find a song by name
+#   can reset the songs that have been created
+
 class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 end
