91e99dd88be60b01370e228a4dfbd36f08faae59
Douglas Obara <douglasobara97@gmail.com>
2022-08-25 06:37:22 +0300

    complete assignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,24 @@
-require 'pry'
+# Artist
+# can be initialized
+# can have a name
+# converts its name to a url friendly parameter
+# Class methods
+#   keeps track of the artists that have been created
+#   can count how many artists have been created
+#   can find an artist by name
+#   can reset the artists that have been created
+# with songs
+#   can have a song added
+#   knows how many songs it has
 
 class Artist
   attr_accessor :name
   attr_reader :songs
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@artists = []
 
@@ -19,24 +35,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
